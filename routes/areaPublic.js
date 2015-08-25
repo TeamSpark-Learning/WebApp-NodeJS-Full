048811c91afef50696a0b032dc1b61e7b15e0052
Anton Boyko <boyko.ant@live.com>
2015-08-25 17:06:39 +0300

    create public pages - index, menu, locations

--- routes/areaPublic.js
+++ routes/areaPublic.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 	var model = {
 		title: 'public area'
 	};
 
 	res.render('public/index', model);
+});
+
+router.get('/locations', function(req, res, next) {
+	var model = {
+		title: 'public area'
+	};
+
+	res.render('public/locations', model);
+});
+
+router.get('/menu', function(req, res, next) {
+	var model = {
+		title: 'public area'
+	};
+
+	res.render('public/menu', model);
 });
 
 module.exports = router;
