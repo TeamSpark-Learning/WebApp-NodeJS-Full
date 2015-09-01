f5f4c51dfdc00d63bc367e382b1f7ceb74df7350
Anton Boyko <boyko.ant@live.com>
2015-09-01 16:48:07 +0300

    introduce layout, header, footer

--- routes/areaPublic.js
+++ routes/areaPublic.js
@@ -1,26 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-	var model = {
-		title: 'public area'
+var getModelBase = function() {
+	return {
+		partials: {
+			header: 'public/partial/header',
+			footer: 'public/partial/footer',
+			layout: 'public/partial/layout'
+		}
 	};
+};
+
+router.get('/', function(req, res, next) {
+	var model = getModelBase();
+
+	model.title = 'public area';
 
 	res.render('public/index', model);
 });
 
 router.get('/locations', function(req, res, next) {
-	var model = {
-		title: 'public area'
-	};
+	var model = getModelBase();
+
+	model.title = 'locations';
 
 	res.render('public/locations', model);
 });
 
 router.get('/menu', function(req, res, next) {
-	var model = {
-		title: 'public area'
-	};
+	var model = getModelBase();
+
+	model.title = 'menu';
 
 	res.render('public/menu', model);
 });
