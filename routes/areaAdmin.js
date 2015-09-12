01c102fc2dd28314f2a9db866c2cae7c13df3991
Anton Boyko <boyko.ant@live.com>
2015-09-12 10:44:16 +0300

    introduce admin area pages

--- routes/areaAdmin.js
+++ routes/areaAdmin.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	var model = {
-		title: 'admin area'
+function getAdminModelBase() {
+	return {
+		partials: {
+			header: 'admin/partial/header',
+			footer: 'admin/partial/footer',
+			layout: 'admin/partial/layout'
+		}
 	};
+};
+
+router.get('/', function(req, res, next) {
+	var model = getAdminModelBase();
+	
+	model.title = 'admin area';
+
+	res.render('admin/index', model);
+});
+
+router.get('/login', function(req, res, next) {
+	var model = getAdminModelBase();
 
 	res.render('admin/login', model);
 });
+
+router.get('/locations', function(req, res, next) {
+	var model = getAdminModelBase();
+
+	res.render('admin/locations', model);
+});
+
+router.get('/menu', function(req, res, next) {
+	var model = getAdminModelBase();
+
+	res.render('admin/menu', model);
+});
+
+
 
 module.exports = router;
